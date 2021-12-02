939afee1fd91f6fdf1e5ca0f4ee3aaf2ed3cba5b
Brasseur Thibault <t.brasseur@activigie.com>
2021-12-02 02:39:19 +0100

    Commit Three : Creation of : components/index.js {NavBar, SideMenu, List} / using data in : components/data.js

--- src/app/index.js
+++ src/app/index.js
@@ -1,8 +1,51 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import { Navbar, List } from "./components";
+import "./styles/app.css";
+import { list } from "./data" 
+
+const SideMenu = ({ loadCategory, category })=> {
+  const links = ["Fruits","Légumes","Produits frais","Epicerie","Boissons"];
+
+  return(
+    <div className="col-sm-2 sidebar">
+      <ul>
+        {links.map((link, index)=> {
+          return(<li className={category == index && "active" } key={index} onClick={()=>loadCategory(index)}>{link}</li>)
+        })}
+      </ul>
+    </div>
+  )
+}
 
 const App = () => {
+  const [ category, setCategory ] = useState(0);
+  const loadCategory = (i) => {
+    setCategory(i)
+  }
+  const filterResults = (input) => {
+    let fullList = list.flat();
+    let filtered = fullList.filter(item => {
+      const name = item.name.toLowerCase();
+      const tern =  input.toLowerCase();  
+      return name.indexOf(tern) > -1
+    })
+    console.log(filtered)
+  }
+  
   return (
-    <Fragment>No products in the store :(</Fragment>
+    <Fragment>
+      <Navbar filter={filterResults}/>
+      <div className="container">
+        <div className="row">
+          <SideMenu loadCategory={loadCategory}  category={category}/>
+          <div className="col-sm">
+            <div className="row">
+              <List data={list[category]} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 export default App;
