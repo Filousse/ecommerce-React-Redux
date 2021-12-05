4ad99c2a8be7d074c61b04b7c0f7e758b65ba8db
Brasseur Thibault <t.brasseur@activigie.com>
2021-12-05 17:15:24 +0100

    sixth commit : REACT REDUX STORE INIT TEMPLATE (in console.log)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './app/pages/App';
+import { store } from "./app/lib/store"
+import { Provider } from "react-redux"
+import { addToCart } from "./app/lib/actions"
 
-ReactDOM.render(<App />, document.getElementById("root")
+console.log(store.getState())
+const unsubscribe = store.subscribe(()=> console.log("Store => ",store.getState()))
+store.dispatch(addToCart({name:"citron"}, 2))
+store.dispatch(addToCart({name:"Kiwi"}, 5))
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById("root")
+
 );
 
